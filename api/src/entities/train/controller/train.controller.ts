038dc8091f11b27a1a22a05ccd5a284e63eeff10
AxelDemorest <axel.demorest@gmail.com>
2022-11-10 23:35:37 +0100

    feat(api): add routes to manage trains

--- api/src/entities/train/controller/train.controller.ts
+++ api/src/entities/train/controller/train.controller.ts
@@ -1,22 +1,27 @@
-import { Body, Controller, HttpStatus, Post, Get, Res } from "@nestjs/common";
-import { createTrainDTO, TrainDTO } from "../models/train.dto";
-import { TrainService } from "../service/train.service";
+import { Body, Controller, HttpStatus, Post, Get, Res } from '@nestjs/common';
+import { createTrainDTO } from '../models/train.dto';
+import { TrainService } from '../service/train.service';
 
 @Controller('train')
 export class TrainController {
-    constructor(private readonly trainService: TrainService) {}
+  constructor(private readonly trainService: TrainService) {}
 
-    @Post('/train')
-    async createTrains(@Res() res, @Body() trains: createTrainDTO[]) {
-        const createTrains = await this.trainService.createTrains(trains);
-        return res.status(HttpStatus.OK).json({
-            message: 'Trains has been submitted successfully!',
-            post: createTrains,
-        });
-    }
+  @Post('/train')
+  async createTrains(@Res() res, @Body() trains: createTrainDTO[]) {
+    const createTrains = await this.trainService.createTrains(trains);
+    return res.status(HttpStatus.OK).json({
+      message: 'Trains has been submitted successfully!',
+      post: createTrains,
+    });
+  }
 
-    @Get('/train')
-    async getTrains() {
-        return this.trainService.getAllTrains();
-    }
+  @Get('/train')
+  async getAllTrainsWithoutSupportedGroups() {
+    return this.trainService.getAllTrainsWithoutSupportedGroups();
+  }
+
+  @Get('/train_groups_supported')
+  async getAllTrainsWithSupportedGroups() {
+    return this.trainService.getAllTrainsWithSupportedGroups();
+  }
 }
--- api/src/entities/train/service/train.service.ts
+++ api/src/entities/train/service/train.service.ts
@@ -1,52 +1,75 @@
-import { Injectable } from "@nestjs/common";
-import { InjectDataSource, InjectRepository } from "@nestjs/typeorm";
-import { GroupType } from "src/entities/group/models/group.entity";
-import { DataSource, Repository } from "typeorm";
-import { createTrainDTO, TrainDTO } from "../models/train.dto";
-import { Train } from "../models/train.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { createTrainDTO } from '../models/train.dto';
+import { Train } from '../models/train.entity';
 
 @Injectable()
 export class TrainService {
-    constructor(
-        @InjectRepository(Train) private trainsRepository: Repository<Train>,
-        @InjectDataSource() private dataSource: DataSource,
-    ) {}
+  constructor(
+    @InjectRepository(Train) private trainsRepository: Repository<Train>,
+    @InjectDataSource() private dataSource: DataSource,
+  ) {}
 
-    async createTrains(trains: createTrainDTO[]) {
+  async createTrains(trains: createTrainDTO[]) {
+    const createTrains = [];
 
-        const createTrains = [];
+    const enumGroupType = {
+      AUT: 'autonome',
+      ENF: 'enfant',
+      HAN: 'handicapé',
+    };
 
-        const enumGroupType = {
-            'AUT': 'autonome',
-            'ENF': 'enfant',
-            'HAN': 'handicapé'
-        }
-        
-        for (const element of trains) {
-            let foundTrain = createTrains.find(row => row.train_number === element.train_number);
-            let { train_number, train_hour, train_groups, train_date, ...groupValues }: createTrainDTO = element;
-            let trainElement = {
-                train_number,
-                train_hour,
-                train_groups,
-                train_date
-            };
-            groupValues.group_type = enumGroupType[groupValues.group_type] ? 'non défini' : enumGroupType[groupValues.group_type];
-            if(foundTrain) {
-                foundTrain.train_groups.push(groupValues);
-            } else {
-                trainElement.train_groups = [];
-                trainElement.train_groups.push(groupValues);
-                createTrains.push(trainElement);
-            }
-        };
-        await this.trainsRepository.save(createTrains);
-        return createTrains;
+    for (const element of trains) {
+      const foundTrain = createTrains.find(
+        (row) =>
+          row.train_number === element.train_number &&
+          row.train_date === element.train_date,
+      );
+      const {
+        train_number,
+        train_hour,
+        train_groups,
+        train_date,
+        ...groupValues
+      }: createTrainDTO = element;
+      const trainElement = {
+        train_number,
+        train_hour,
+        train_groups,
+        train_date,
+      };
+      groupValues.group_type = enumGroupType[groupValues.group_type]
+        ? enumGroupType[groupValues.group_type]
+        : 'non défini';
+      if (groupValues.group_meeting_point?.trim().toLowerCase() !== 'non')
+        groupValues.group_is_supported = true;
+      else groupValues.group_is_supported = false;
+      if (foundTrain) {
+        foundTrain.train_groups.push(groupValues);
+      } else {
+        trainElement.train_groups = [];
+        trainElement.train_groups.push(groupValues);
+        createTrains.push(trainElement);
+      }
     }
+    await this.trainsRepository.save(createTrains);
+    return createTrains;
+  }
 
-    async getAllTrains(): Promise<Train[]> {
-        return await this.trainsRepository.find({
-            relations: ['train_groups'],
-        });
-      }   
+  async getAllTrainsWithoutSupportedGroups(): Promise<Train[]> {
+    return this.trainsRepository
+      .createQueryBuilder('train')
+      .leftJoinAndSelect('train.train_groups', 'group')
+      .where('group.group_is_supported = false')
+      .getMany();
+  }
+
+  async getAllTrainsWithSupportedGroups(): Promise<Train[]> {
+    return this.trainsRepository
+      .createQueryBuilder('train')
+      .leftJoinAndSelect('train.train_groups', 'group')
+      .where('group.group_is_supported = true')
+      .getMany();
+  }
 }
