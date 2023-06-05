050b000475d7c57762dbd87ba7d66def933cfeac
AxelWebDeveloper <axel.demorest.pro@gmail.com>
2023-06-05 17:58:31 +0200

    yarn.lock error

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   const port = process.env.PORT || 3001;
