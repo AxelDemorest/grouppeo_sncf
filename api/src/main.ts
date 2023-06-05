db2a94c14a39182d8eb6c234d32808d691ed817b
AxelWebDeveloper <axel.demorest.pro@gmail.com>
2023-06-05 17:44:10 +0200

    cors error

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   const port = process.env.PORT || 3001;
